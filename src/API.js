a555322a8142b3fe6db3861ed2f817a82295ddb3
Rio <riostockton@gmail.com>
2021-03-16 17:27:06 +0000

    Added encryption

--- src/API.js
+++ src/API.js
@@ -1,10 +1,11 @@
-import { COLLECTION_ID, API_KEY } from "./modules/private.js";
 import axios from "axios";
 
 // Fetch unsplash API image data
 const fetchBackgroundImageData = async () => {
   try {
-    const apiURL = `https://api.unsplash.com/collections/${COLLECTION_ID}/photos?per_page=60&client_id=${API_KEY}`;
+    const apiURL = `
+      https://api.unsplash.com/collections/${process.env.REACT_APP_COLLECTION_ID}/photos?per_page=60&client_id=${process.env.REACT_APP_API_KEY}
+    `;
     const { data } = await axios.get(apiURL);
     return data;
   } catch(err) {
